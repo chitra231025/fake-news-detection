8e444ddc9404fa11e6aa868d3c0fb32f9fad55de
Chitra Ameta <chitraameta@Chitras-MacBook-Air.local>
2025-12-18 14:56:57 +0530

    Updated app.py and requirements for HuggingFace deployment

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import joblib
 
-# Load model + vectorizer
+# Load model and vectorizer
 model = joblib.load("fake_news_model.pkl")
 vectorizer = joblib.load("vectorizer.pkl")
 
 st.title("📰 Fake News Detection")
-
 st.write("Enter a news headline and click Predict.")
+
 # Text Input
 user_input = st.text_input("News text:")
 
@@ -16,10 +16,10 @@ if st.button("Predict"):
     if user_input.strip() == "":
         st.warning("Please enter some text.")
     else:
-        x = vectorizer.transform([user_input])
-        pred = model.predict(x)[0]   # "fake" or "real"
+        X = vectorizer.transform([user_input])
+        pred = model.predict(X)[0]   # 0 or 1
 
-        if pred == "fake":
+        if pred == 1:
             st.error("Fake News")
         else:
             st.success("Real News")
